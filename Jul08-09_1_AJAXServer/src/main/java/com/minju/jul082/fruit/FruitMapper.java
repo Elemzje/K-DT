aa9e01e4f53ff04162603a6eafb681b9d0f25f8c
Elemzje <minjulv06101@gmail.com>
2024-07-09 16:15:19 +0900

    Spring AJAX
    AJAX XML, JSON

---
+++ Jul08-09_1_AJAXServer/src/main/java/com/minju/jul082/fruit/FruitMapper.java
@@ -0,0 +1,8 @@
+package com.minju.jul082.fruit;
+
+import java.util.List;
+
+public interface FruitMapper {
+	public abstract List<Fruit> getAllFruit();
+	public abstract List<Fruit> searchFruit(Fruit f);
+}
