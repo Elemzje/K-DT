5ebbc7d5d7af17b022bfd25b61f7f60f203edd01
Elemzje <minjulv06101@gmail.com>
2024-07-31 15:11:53 +0900

    MongoDB
    Python Connect

---
+++ Jul29_1_MongoDB/p03_select.py
@@ -0,0 +1,28 @@
+#  -*- coding:utf-8 -*-
+from pymongo.mongo_client import MongoClient
+
+con = MongoClient("192.168.0.13")
+
+db = con.jul29
+
+s = db.jul29_lunch.find()
+# print(s)
+
+for l in s:
+    print(l["_id"])
+    print(l["price"])
+    print("--------------")
+
+
+con.close()
+print("Success !")
+
+
+
+
+
+
+
+
+
+
